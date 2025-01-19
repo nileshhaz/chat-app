9e946e853c41568ee8ebdfc1ddf775025df8fad2
Nilesh Hazra <nileshhazra.nh@gmail.com>
2025-01-19 16:58:52 +0530

    Fix case sensitivity and rebuild project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/socketContext.jsx'
+import { SocketContextProvider } from './context/socketContext.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
