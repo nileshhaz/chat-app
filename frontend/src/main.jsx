ecfc5640dfa00de46ca3f2035f1bf12093ed4f24
coldBlood <85475503+nileshhaz@users.noreply.github.com>
2025-01-19 17:01:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/socketContext.jsx';
+import { SocketContextProvider } from './context/SocketContext.jsx';
 
 
 createRoot(document.getElementById('root')).render(
